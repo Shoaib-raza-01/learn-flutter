71995121ebe3434b01e262733a745d0965cb532a
Shoaib-raza-01 <razashoaib270@gmail.com>
2021-10-31 20:55:13 +0530

    loginPage

--- my_app/lib/screens/homepage.dart
+++ my_app/lib/screens/homepage.dart
@@ -12,68 +12,75 @@ class HomePage extends StatelessWidget {
         title: const Text("App1"),
         // backgroundColor: Colors.black12,
       ),
-      body:Column(
-        children:const <Widget> [
-          Padding(
-            padding:  EdgeInsets.all(8.0),
-            child:   Center(
-              child:  Text(
+      body: Column(
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
                 " HELLO",
                 style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 20,
-                  fontStyle: FontStyle.italic,
-                  fontWeight: FontWeight.bold,
-                  backgroundColor: Colors.pink,
-                  shadows: [
-                    Shadow(
-                      color: Colors.black,
-                      offset: Offset(2.0, 4.0),
-                    ),
-                    
-                  ]
-                ),
+                    color: Colors.blue,
+                    fontSize: 20,
+                    fontStyle: FontStyle.italic,
+                    fontWeight: FontWeight.bold,
+                    backgroundColor: Colors.pink,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black,
+                        offset: Offset(2.0, 4.0),
+                      ),
+                    ]),
               ),
             ),
           ),
-          TextField(
-            decoration: InputDecoration(
+          TextFormField(
+            decoration: const InputDecoration(
               border: InputBorder.none,
-
               labelText: 'Name',
               hintText: 'Enter your name : ',
             ),
           ),
-          TextField(
-            decoration: InputDecoration(
+          TextFormField(
+            obscureText: true,
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              labelText: 'Password',
+              hintText: 'Enter your password : ',
+            ),
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
               border: InputBorder.none,
               labelText: 'Age',
               hintText: 'Enter your Age : ',
             ),
           ),
-          TextField(
-            decoration: InputDecoration(
+          TextFormField(
+            decoration: const InputDecoration(
               border: InputBorder.none,
               labelText: 'Class',
               hintText: 'Enter your class : ',
             ),
           ),
-          TextField(
-            decoration: InputDecoration(
+          TextFormField(
+            decoration: const InputDecoration(
               border: InputBorder.none,
-              
               labelText: 'Section',
               hintText: 'Enter your section : ',
             ),
           ),
+          ElevatedButton(
+            child: const Text("Submit"),
+            style: TextButton.styleFrom(),
+            onPressed: () {
+              print("shoaib");
+            },
+          )
         ],
-        
-      ), 
-      
-      
-      drawer:const Drawer(
-
-         child:   Text('home'),
+      ),
+      drawer: const Drawer(
+        child: Text('home'),
       ),
       backgroundColor: Colors.grey,
     );
